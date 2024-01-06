ea51d6225bf790ed2d8173e7a07a35e95fcc1adc
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2024-01-06 23:58:55 +0800

    v0.1.1: Initial commit, implementing concurrent downloading gallery.

---
+++ usage/getting_started.py
@@ -0,0 +1,9 @@
+from exhentai import ExhentaiOption
+
+# start with creating a option object through a specific file
+option = ExhentaiOption.from_file('../option/all_in_one.yml')
+# equivalent to below: by multiple files in a dir
+# option = ExhentaiOption.from_dir('../option/')
+
+# call download_gallery method
+option.download_gallery_by_url('https://exhentai.org/g/2768121/e281882c85/')
